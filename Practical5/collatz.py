c6480b8872857bc775fa93026951e97fc5fe7fbc
Wang Weijian <Weijian.19@intl.zju.edu.cn>
2020-03-11 22:37:30 +0800

    Add homework

---
+++ Practical5/collatz.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 09:58:19 2020
+
+@author: 蒋冰
+"""
+
+# creat n
+import random
+n = random.randint(1,100)
+# use while to do classified discussion
+#when number is even
+while n!=1:
+    if n%2==0:
+        n=n/2
+        print(n)
+#when number is odd
+    else:
+        n=3*n+1
+        print(n)
+        
+        
+        
+        
+
+       
+            
+    
+
+
+
