c0bfd171c3e744586abb9e7adfda1abebefd862b
Wang Weijian <Weijian.19@intl.zju.edu.cn>
2020-03-31 20:29:56 +0800

    Practical5

--- Practical5/untitled0.py
+++ Practical5/untitled0.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 18 17:47:59 2020
+Created on Mon Mar 30 19:46:08 2020
 
 @author: 蒋冰
 """
+import re
+x = 'From robot@zju.edu.cn Feb 10:38:06 2019 '
+y = re.findall(r'^From (\S+@)',x)
+print(y)
 
-import numpy as np
-import matplotlib.pyplot as plt
-scores =(25,75,75,25)
-std = (2,3,4,1)
-N=4
-ind=np.arange(N)
-width=0.5
-p1= plt.bar(ind,scores,width,yerr=std)
-plt.ylabel('percentage of attacked models')
-plt.title('beach habitat              inland habitat')
-plt.xticks(ind,('light models','dark models','light models','dark models'))
-plt.yticks(np.arange(0,101,10))
-plt.show()
+import re
+s = input()
+'abcdefghi_def'
+result = re.sub(r'abc(def)ghi', r'\1',s)
+print(result)
+
